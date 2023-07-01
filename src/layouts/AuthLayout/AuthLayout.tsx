81a0717d15611f0eebd848f6b0ab4adb7b1f047d
Adrian Changalombo <alextravc.342@gmail.com>
2023-07-01 15:55:37 -0500

    [ADD]: auth layout init design

--- src/layouts/AuthLayout/AuthLayout.tsx
+++ src/layouts/AuthLayout/AuthLayout.tsx
@@ -4,9 +4,13 @@ export interface AuthLayoutProps {}
 
 const AuthLayout: React.FC<AuthLayoutProps> = () => {
   return (
-    <div>
-      <Outlet />
-    </div>
+    <main className="max-w-4xl m-auto mt-10 md:mt-28 flex flex-col md:flex-row items-center">
+      <img src="../img/logo.svg" alt="Logo image" className="max-w-xs" />
+
+      <div className="p-10 w-full">
+        <Outlet />
+      </div>
+    </main>
   );
 };
 
