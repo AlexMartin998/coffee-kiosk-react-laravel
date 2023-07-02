2b38cfb36d8f2d6b424790feefb77360bada21c4
Adrian Changalombo <alextravc.342@gmail.com>
2023-07-02 14:35:45 -0500

    [ADD]: close product modal

--- src/KioskApp/components/ui/ProductModal/ProductModal.tsx
+++ src/KioskApp/components/ui/ProductModal/ProductModal.tsx
@@ -21,7 +21,7 @@ Modal.setAppElement('#root');
 
 const ProductModal: React.FC<ProductModalProps> = () => {
   const { isProductModalOpen, closeProductModal } = useUI();
-  const { setActiveProduct } = useKiosk();
+  const { setActiveProduct, activeProduct } = useKiosk();
 
   const handleCloseModal = () => {
     closeProductModal();
@@ -30,11 +30,35 @@ const ProductModal: React.FC<ProductModalProps> = () => {
 
   return (
     <Modal isOpen={isProductModalOpen} style={customStyles}>
-      <h1>From Modal</h1>
+      <div className="md:flex gap-10">
+        <div className="md:w-1/3">
+          <img
+            src={`/img/${activeProduct.image}.jpg`}
+            alt={`${activeProduct.name} image`}
+          />
+        </div>
 
-      <button type="button" onClick={handleCloseModal}>
-        Close
-      </button>
+        <div className="md:w-2/3">
+          <div className="flex justify-end">
+            <button type="button" onClick={handleCloseModal}>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className="w-6 h-6"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M9.75 9.75l4.5 4.5m0-4.5l-4.5 4.5M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+                />
+              </svg>
+            </button>
+          </div>
+        </div>
+      </div>
     </Modal>
   );
 };
